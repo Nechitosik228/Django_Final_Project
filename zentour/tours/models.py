63ff0f119aca40889e0e186a2be741f1e93403af
Nechitosik228 <nikitanechitailo93@gmail.com>
2025-07-04 15:40:18 +0200

    create OrderItem model

--- zentour/tours/models.py
+++ zentour/tours/models.py
@@ -79,3 +79,18 @@ class Order(models.Model):
     @property
     def total(self):
         return sum([item.item_total for item in self.items.all()])
+    
+
+class OrderItem(models.Model):
+
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, name='items')
+    tour = models.ForeignKey(Tour, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=1)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+
+    @property
+    def item_total(self):
+        return self.tour.discount_price * self.amount
+    
+    def __str__(self):
+        return f"{self.order.id} : {self.tour.name} : {self.amount} : ${self.price}"
