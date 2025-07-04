a623724851ab4176d5d617d9df99bdd167fc3a28
Nechitosik228 <nikitanechitailo93@gmail.com>
2025-07-04 15:50:31 +0200

    add __str__

--- zentour/accounts/models.py
+++ zentour/accounts/models.py
@@ -10,3 +10,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to="avatars/", blank=True)
     balance = models.OneToOneField(Balance, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
