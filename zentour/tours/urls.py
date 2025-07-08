7d3a557cd1703df9d7b1924defa64cd87dffc84b
Nechitosik228 <nikitanechitailo93@gmail.com>
2025-07-08 19:27:48 +0200

    create homepage

---
+++ zentour/tours/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import (
+    home
+)
+
+
+app_name = "tours"
+
+
+urlpatterns = [
+    path("home/", home, name="home")
+]
--- zentour/tours/views.py
+++ zentour/tours/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Tour
+
+
+def home(request):
+    tours = Tour.objects.all()
+
+    filter = request.GET.get("filter")
+    min_price = request.GET.get("min_price")
+    max_price = request.GET.get("max_price")
+    start_date = request.GET.get("start_date")
+    end_date = request.GET.get("end_date")
+
+    if min_price:
+        tours = tours.filter(price__gte=min_price)
+    if max_price:
+        tours = tours.filter(price__lte=max_price)
+
+    if start_date:
+        tours = tours.filter(start_date__gte=start_date)
+    if end_date:
+        tours = tours.filter(end_date__lte=end_date)
+
+    if filter == "decrease_price":
+        tours = tours.order_by("-price")
+    elif filter == "increase_price":
+        tours = tours.order_by("price")
+    elif filter == "increase_rating":
+        tours = tours.order_by("rating")
+    elif filter == "decrease_rating":
+        tours = tours.order_by("-rating")
+
+    return render(request, 'home.html', context={'tours': tours})
