c1b4312fc2bed6854907d0f6ca631596bc3fb060
Yar00myr <ivanucaromir@gmail.com>
2025-07-11 23:57:29 +0300

    add  view, templates, add to models __str__ ...

--- zentour/accounts/models.py
+++ zentour/accounts/models.py
@@ -5,10 +5,13 @@ from django.contrib.auth.models import User
 class Balance(models.Model):
     amount = models.DecimalField(default=0, max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return f"Ballance: {self.amount}"
+
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to="avatars/", blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
     balance = models.OneToOneField(Balance, on_delete=models.CASCADE)
 
     def __str__(self):
