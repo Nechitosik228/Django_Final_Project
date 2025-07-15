2cf45fe30db10ec9145d8e1deda76aa23e6527d2
Yar00myr <ivanucaromir@gmail.com>
2025-07-15 23:58:58 +0300

    reset password add .env.example update requirements.txt

--- zentour/accounts/urls.py
+++ zentour/accounts/urls.py
@@ -26,4 +26,20 @@ urlpatterns = [
         ),
         name="password_change_done",
     ),
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="accounts/reset_password/form.html",
+            email_template_name="accounts/emails/reset.html",
+            success_url=reverse_lazy("accounts:password_reset_done"),
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="accounts/reset_password/done.html"
+        ),
+        name="password_reset_done",
+    ),
 ]
