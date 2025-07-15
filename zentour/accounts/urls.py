3361878b2c09f25ad4f3816945f7425779b412b6
Yar00myr <ivanucaromir@gmail.com>
2025-07-15 19:45:47 +0300

    change password

--- zentour/accounts/urls.py
+++ zentour/accounts/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
 
 from .views import register, login_view, logout_view, profile, edit_user_profile
 
@@ -10,4 +11,19 @@ urlpatterns = [
     path("logout/", logout_view, name="logout"),
     path("profile/", profile, name="profile"),
     path("edit_profile/", edit_user_profile, name="edit_profile"),
+    path(
+        "password_change/",
+        auth_views.PasswordChangeView.as_view(
+            success_url=reverse_lazy("accounts:password_change_done"),
+            template_name="accounts/password_change.html",
+        ),
+        name="password_change",
+    ),
+    path(
+        "password_change/done/",
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name="accounts/password_change_done.html"
+        ),
+        name="password_change_done",
+    ),
 ]
