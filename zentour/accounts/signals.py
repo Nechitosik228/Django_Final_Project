858ff10df24b72ec9ef30ec92dbeba5f91c91552
Yar00myr <ivanucaromir@gmail.com>
2025-07-18 10:38:26 +0300

    remove same code from signals, and add migration

--- zentour/accounts/signals.py
+++ zentour/accounts/signals.py
@@ -12,15 +12,3 @@ def create_user_profile_and_cart_and_balance(sender, instance, created, **kwargs
         balance = Balance.objects.create()
         Cart.objects.create(user=instance)
         Profile.objects.create(user=instance, balance=balance)
-
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.contrib.auth.models import User
-from .models import Profile, Balance
-
-
-@receiver(post_save, sender=User)
-def create_user_profile_and_cart(sender, instance, created, **kwargs):
-    if created:
-        balance = Balance.objects.create()
-        Profile.objects.create(user=instance, balance=balance)
