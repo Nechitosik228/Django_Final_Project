46e8c125d799b57e4ff336df9b9c5474c1d2ea52
Nechitosik228 <nikitanechitailo93@gmail.com>
2025-07-23 16:47:35 +0300

    write tests with tour, tour with discount, with no tickets

--- zentour/tours/test/fixtures.py
+++ zentour/tours/test/fixtures.py
@@ -8,9 +8,39 @@ def tour(user):
         user=user,
         name='Test name',
         description='Test description',
-        start_date='01.01.2025',
-        end_date='02.01.2025',
+        start_date='2025-01-01',
+        end_date='2025-02-01',
         price=50,
         tickets_amount=20,
         cities='Berlin'
+    )
+
+
+@pytest.fixture
+def tour_with_discount(user):
+    return Tour.objects.create(
+        user=user,
+        name='Test2 name',
+        description='Test description',
+        start_date='2025-01-01',
+        end_date='2025-02-01',
+        price=50,
+        discount=10,
+        tickets_amount=20,
+        cities='Berlin'
     ) 
+
+
+@pytest.fixture
+def tour_with_no_tickets(user):
+    return Tour.objects.create(
+        user=user,
+        name='Test3 name',
+        description='Test description',
+        start_date='2025-01-01',
+        end_date='2025-02-01',
+        price=50,
+        discount=10,
+        tickets_amount=0,
+        cities='Berlin'
+    )
--- zentour/tours/test/test_models.py
+++ zentour/tours/test/test_models.py
@@ -5,4 +5,24 @@ from django.urls import reverse
 
 from accounts.models import Profile
 from tours.models import Cart, Tour, Review, CartItem, Order, OrderItem
-from .fixtures import tour
+from .fixtures import tour, tour_with_discount, tour_with_no_tickets
+
+
+@pytest.mark.django_db
+def test_tour_model(tour):
+    assert tour.discount == 0
+    assert tour.rating == None
+    assert tour.discount_price == tour.price
+    assert tour.available == True
+
+
+@pytest.mark.django_db
+def test_tour_with_discount_model(tour_with_discount):
+    assert tour_with_discount.discount == 10
+    assert tour_with_discount.discount_price == 45
+
+
+@pytest.mark.django_db
+def test_tour_with_no_tickets_model(tour_with_no_tickets):
+    assert tour_with_no_tickets.tickets_amount == 0
+    assert tour_with_no_tickets.available == False
