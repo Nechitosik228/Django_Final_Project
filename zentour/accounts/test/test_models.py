ef7dc25c1aa377dd875b1cc332d34059626c012e
Yar00myr <ivanucaromir@gmail.com>
2025-07-26 19:47:11 +0300

    add test

--- zentour/accounts/test/test_models.py
+++ zentour/accounts/test/test_models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 from accounts.models import Balance, Transaction, Profile
 from tours.models import Cart, Order, Review, BoughtTour
-from .fixtures import profile
+from .fixtures import profile, balance
 
 
 @pytest.mark.django_db
@@ -28,3 +28,15 @@ def test_profile_creation(user):
     assert balance.amount == 0
     assert cart.items.count() == 0
 
+
+@pytest.mark.django_db
+def test_balance_top_up_and_save(user):
+    balance = user.profile.balance
+
+    assert balance.amount == 0
+
+    balance.amount += 150
+    balance.save()
+
+    updated_balance = Balance.objects.get(id=balance.id)
+    assert updated_balance.amount == 150
