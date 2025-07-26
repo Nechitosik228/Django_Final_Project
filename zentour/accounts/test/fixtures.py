b95cd19fad2f6dda9d1ae656efcdb062ce939cfd
Yar00myr <ivanucaromir@gmail.com>
2025-07-26 20:12:46 +0300

    write more tests

--- zentour/accounts/test/fixtures.py
+++ zentour/accounts/test/fixtures.py
@@ -10,3 +10,14 @@ def profile(user):
 @pytest.fixture
 def balance(profile):
     return profile.balance
+
+
+@pytest.fixture
+def transaction(balance):
+    return Transaction.objects.create(
+        balance=balance,
+        action=Transaction.Action.TOPING_UP,
+        status=Transaction.Choices.NEW,
+        money_amount=100,
+        category="Test category",
+    )
