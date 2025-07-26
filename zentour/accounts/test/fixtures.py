300572bed4883cdcc84dceb9935d231371278390
Yar00myr <ivanucaromir@gmail.com>
2025-07-26 19:47:11 +0300

    add test

--- zentour/accounts/test/fixtures.py
+++ zentour/accounts/test/fixtures.py
@@ -5,3 +5,8 @@ from accounts.models import Balance, Transaction, Profile
 @pytest.fixture
 def profile(user):
     return user.profile
+
+
+@pytest.fixture
+def balance(profile):
+    return profile.balance
