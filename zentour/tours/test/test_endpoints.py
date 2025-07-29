44afa2401ba90aa4c2c5dd7c45563a954046a0c0
Nechitosik228 <nikitanechitailo93@gmail.com>
2025-07-29 19:52:44 +0200

    make some tests, change tour creation form

--- zentour/tours/test/test_endpoints.py
+++ zentour/tours/test/test_endpoints.py
@@ -3,6 +3,8 @@ import datetime
 
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.contrib.messages import get_messages
+from django.utils.http import urlencode
 
 from accounts.models import Profile
 from tours.models import Cart, Tour, Review, CartItem, Order, OrderItem
@@ -13,3 +15,60 @@ from .fixtures import (
     cart_item,
     cart_item_with_discount,
 )
+
+
+@pytest.mark.django_db
+def test_create_tour_endpoint_not_authorized(client):
+    url = reverse("tours:create_tour")
+
+    form_data = {
+        "name": "Test name",
+        "description": "Test description",
+        "price": 1,
+        "tickets_amount": 2,
+        "cities": "Berlin",
+        "start_date": "2025-08-30",
+        "end_date": "2025-09-30",
+    }
+
+    response = client.post(url, data=form_data)
+
+    url = reverse('accounts:login')
+    url_with_query = f"{url}?next=/tours/create-tour/"
+
+    assert response.status_code == 302
+    assert response.url == url_with_query
+    assert not Tour.objects.filter(name='Test name')
+
+
+# @pytest.mark.django_db
+# def test_create_tour_endpoint(client, super_user):
+#     client.login(username='Admin', password='Adminpasword123')
+
+#     url = reverse("tours:create_tour")
+
+#     form_data = {
+#         "name": "Test name",
+#         "description": "Test description",
+#         "price": 1,
+#         "tickets_amount": 2,
+#         "cities": "Berlin",
+#         "start_date": "2025-08-30",
+#         "end_date": "2025-09-30",
+#     }
+
+#     response = client.post(url, data=form_data)
+
+#     messages = [m.error for m in get_messages(response.wsgi_request)]
+#     print(messages)
+#     print(response.serialize_headers())
+#     assert response.status_code == 200
+
+
+# @pytest.mark.django_db
+# def test_get_tour_endpoint(client, tour):
+#     url = reverse('tours:tour_detail', args=[tour.id])
+
+#     response = client.get(url)
+
+#     assert response.status_code == 200
