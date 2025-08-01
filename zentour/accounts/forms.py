4203b5ee3c27ab1228cc806d9ff80fb3038dd018
Nechitosik228 <nikitanechitailo93@gmail.com>
2025-08-01 10:37:55 +0200

    add email verification

--- zentour/accounts/forms.py
+++ zentour/accounts/forms.py
@@ -1,4 +1,7 @@
+import requests
+
 from django import forms
+from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UsernameField
@@ -6,15 +9,30 @@ from django.contrib.auth.forms import UserCreationForm, UsernameField
 from .models import Balance
 
 
+url = settings.API_EMAIL_VERIFIER_URL
+
+
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True, help_text="Enter your email")
 
     def clean_email(self):
-        new_email = self.cleaned_data.get("email")
-        if User.objects.filter(email=new_email).exists():
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email=email).exists():
             raise forms.ValidationError("This email already exists")
+        response = requests.get(
+            url=url,
+            params={
+                "access_key": settings.API_KEY,
+                "smtp": "1",
+                "format": "1",
+                "email": email,
+            },
+        )
+        print(response.json())
+        if response.json().get("smtp_check") == False:
+            raise forms.ValidationError("This email doesn't exist")
         else:
-            return new_email
+            return email
 
     class Meta:
         model = User
